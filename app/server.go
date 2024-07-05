e8330dd9dba2c01f7b4ae0ad2d4ace1146e8d6d4
sp3dr4 <pedra89@gmail.com>
2024-07-05 21:53:18 +0200

    parse request line to inspect path

--- app/server.go
+++ app/server.go
@@ -1,28 +1,70 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
+var logger = log.New(os.Stdout, "> ", log.Ldate|log.Ltime|log.Lmicroseconds)
+
 func main() {
-	fmt.Println("Logs from your program will appear here!")
+	logger.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		logger.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		logger.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	if _, err = fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n\r\n"); err != nil {
-		fmt.Println("Error writing response to connection: ", err.Error())
+	reader := bufio.NewReader(conn)
+	reqLine, err := parseRequest(reader)
+	if err != nil {
+		logger.Println("Error parsing request: ", err.Error())
 		os.Exit(1)
 	}
+	logger.Printf("request line: %v\n", reqLine)
+
+	resp, err := handleRequest(reqLine)
+	if err != nil {
+		logger.Println("Error handling request: ", err.Error())
+		os.Exit(1)
+	}
+
+	if _, err = fmt.Fprint(conn, resp); err != nil {
+		logger.Println("Error writing response to connection: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func parseRequest(r *bufio.Reader) ([]string, error) {
+	line, err := r.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	parts := strings.Fields(line)
+
+	return parts, nil
+}
+
+func handleRequest(line []string) (string, error) {
+	if len(line) != 3 {
+		return "", fmt.Errorf("expected 3 parts in request line, got %v", line)
+	}
+	resp := "HTTP/1.1 200 OK\r\n\r\n"
+	if line[1] != "/" {
+		resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	return resp, nil
 }
