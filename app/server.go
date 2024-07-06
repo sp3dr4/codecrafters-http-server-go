c4cf6b6b0cbde839d1548ac046c97f1579181601
sp3dr4 <pedra89@gmail.com>
2024-07-06 14:40:19 +0200

    parse body from request

--- app/server.go
+++ app/server.go
@@ -16,9 +16,23 @@ import (
 
 var logger = log.New(os.Stdout, "> ", log.Ldate|log.Ltime|log.Lmicroseconds)
 
-type request struct {
+type Header struct {
+	name  string
+	value string
+}
+
+type Request struct {
 	reqLine []string
-	headers []string
+	headers []Header
+	body    string
+}
+
+func (r *Request) GetHeader(name string) (string, bool) {
+	ix := slices.IndexFunc(r.headers, func(h Header) bool { return strings.ToLower(name) == h.name })
+	if ix == -1 {
+		return "", false
+	}
+	return r.headers[ix].value, true
 }
 
 type service struct {
@@ -73,45 +87,58 @@ func handleConn(c *net.Conn) {
 	}
 }
 
-func parseRequest(r *bufio.Reader) (request, error) {
+func parseRequest(r *bufio.Reader) (Request, error) {
 	buf := make([]byte, 1024)
-	_, err := r.Read(buf)
+	n, err := r.Read(buf)
 	if err != nil {
-		return request{}, err
+		return Request{}, err
 	}
-	// logger.Printf("n: %d\n", n)
+	logger.Printf("n: %d\n", n)
+	if n == 1024 {
+		return Request{}, fmt.Errorf("didn't handle big request over 1024 bytes")
+	}
 
-	raw := string(buf)
+	raw := string(buf[:n])
+	// logger.Printf("raw:\n%q\n", raw)
 	lines := strings.Split(raw, "\n")
 
-	req := request{
+	req := Request{
 		reqLine: strings.Fields(lines[0]),
-		headers: []string{},
+		headers: []Header{},
+		body:    "",
 	}
 
 	if len(req.reqLine) != 3 {
-		return request{}, fmt.Errorf("expected 3 parts in request line, got %v", req.reqLine)
+		return Request{}, fmt.Errorf("expected 3 parts in request line, got %v", req.reqLine)
 	}
 
 	i := 1
 	for {
 		ln := strings.TrimSpace(lines[i])
 		if ln == "" {
+			i++
 			break
 		}
-		req.headers = append(req.headers, ln)
+		elems := strings.Split(ln, ": ")
+		req.headers = append(req.headers, Header{name: strings.ToLower(elems[0]), value: elems[1]})
 		i++
 	}
+
+	// for j := i; j < len(lines); j++ {
+	// 	logger.Printf("lines.%d: %q\n", j, lines[j])
+	// }
+
+	req.body = lines[i]
 
 	return req, nil
 }
 
-func handleRequest(req request) (string, error) {
+func handleRequest(req Request) (string, error) {
 	logger.Println("handling request: ", req)
 
 	routes := []struct {
 		pattern string
-		handler func(request) (string, error)
+		handler func(Request) (string, error)
 	}{
 		{`^\/files\/\S+$`, svc.getfile},
 		{`^\/echo\/\S+$`, svc.echo},
@@ -143,36 +170,46 @@ func handleRequest(req request) (string, error) {
 	return resp, nil
 }
 
-func (sv *service) echo(req request) (string, error) {
+func (sv *service) echo(req Request) (string, error) {
 	toEcho := strings.Split(req.reqLine[1][1:], "/")[1]
 	return fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(toEcho), toEcho), nil
 }
 
-func (sv *service) useragent(req request) (string, error) {
-	agentIx := slices.IndexFunc(req.headers, func(h string) bool { return strings.HasPrefix(strings.ToLower(h), "user-agent: ") })
-	if agentIx == -1 {
-		return "", fmt.Errorf("user-agent header not found")
+func (sv *service) useragent(req Request) (string, error) {
+	uagent, ok := req.GetHeader("user-agent")
+	if !ok {
+		return "", fmt.Errorf("User-Agent header not found")
 	}
-	agent := strings.Split(req.headers[agentIx], ": ")[1]
-	return fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(agent), agent), nil
+	return fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(uagent), uagent), nil
 }
 
-func (sv *service) root(req request) (string, error) {
+func (sv *service) root(req Request) (string, error) {
 	return "HTTP/1.1 200 OK\r\n\r\n", nil
 }
 
-func (sv *service) getfile(req request) (string, error) {
+func (sv *service) getfile(req Request) (string, error) {
 	filename := strings.Split(req.reqLine[1][1:], "/")[1]
 
-	dat, err := os.ReadFile(fmt.Sprintf("%s%s", sv.directory, filename))
-	if err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			return "HTTP/1.1 404 Not Found\r\n\r\n", nil
+	if req.reqLine[0] == "GET" {
+		dat, err := os.ReadFile(fmt.Sprintf("%s%s", sv.directory, filename))
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				return "HTTP/1.1 404 Not Found\r\n\r\n", nil
+			}
+			logger.Println("Error reading file: ", err.Error())
+			return "", err
 		}
-		logger.Println("Error reading file: ", err.Error())
-		return "", err
+
+		datStr := string(dat)
+		return fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(datStr), datStr), nil
 	}
 
-	datStr := string(dat)
-	return fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(datStr), datStr), nil
+	if req.reqLine[0] == "POST" {
+		if err := os.WriteFile(fmt.Sprintf("%s%s", sv.directory, filename), []byte(req.body), 0666); err != nil {
+			return "", err
+		}
+		return "HTTP/1.1 201 Created\r\n\r\n", nil
+	}
+
+	return "", fmt.Errorf("invalid method %v", req.reqLine[0])
 }
